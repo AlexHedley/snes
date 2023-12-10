c77026c31a35b8f5eded21bd4e930800f028f8ca
Alex Hedley <alex.hedley@me.com>
2023-12-10 20:19:10 +0000

    Update js/css, added boxed games

--- README.md
+++ README.md
@@ -13,6 +13,7 @@
 
 ## Run
 
+`cd src`
 `py -m http.server`
 
 - [local](http://localhost:8000/)
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('myController', function ($scope, $http, $q, $filter) {
+var myApp = angular.module('app', []);
+myApp.controller('controller', function ($scope, $http, $q, $filter) {
 
     $scope.games = [];
 
@@ -13,34 +13,8 @@ myApp.controller('myController', function ($scope, $http, $q, $filter) {
         $http.get(file)
         .then(function(response) {
             $scope.games = response.data.games;
-            $scope.generatePivot();
         });
     };
-
-    $scope.generatePivot = () => {
-        
-        $scope.data = $scope.games.map(game => game.players.filter(player => player.winner === true));
-        var data = [].concat.apply([], $scope.data);
-
-        if ($scope.ui) {
-            $("#output").pivotUI(
-                data,
-                {
-                    rows: ["name"],
-                    cols: ["score"]
-                }
-            );
-        } else {
-            $("#output").pivot(
-                data,
-                {
-                    rows: ["name"],
-                    cols: ["score"]
-                }
-            );
-        }
-
-    }
 
     $scope.init();
 });
